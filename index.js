06033a796160995d0c6132f0854641dc45e2cba0
Samir Dholane <samirdholane01@gmail.com>
2024-12-30 20:11:44 +0530

    index.js file is created and index.html file is modified

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("demo1").innerHTML = "Hello World";
+
+function myfunction() {
+  document.getElementById("lightoff").src = "pic_bulbon.gif";
+}
+
+function myfunction2() {
+  document.getElementById("lightoff").src = "pic_bulboff.gif";
+}
+
+function myfunction3() {
+  document.getElementById("demo").style.display = "block";
+}
